ed6120d25c9b964cb1bcde5e1aaf1f1985bacd25
Alvaro Mena <alvamen@gmail.com>
2023-03-22 19:56:54 -0600

    código visto en clase

--- src/test/java/cr/ac/ucr/paraiso/ie/progra2/tema1/paco/PersonaTest.java
+++ src/test/java/cr/ac/ucr/paraiso/ie/progra2/tema1/paco/PersonaTest.java
@@ -5,7 +5,15 @@ import org.junit.jupiter.api.Test;
 public class PersonaTest {
     @Test
     public void crear_instancias_funciona(){
-
+        Persona persona = new Persona();
+        persona.setNombre("Paco");
+        persona.setNombre("Apellido1 Appellido2");
+        persona.setHorasObservadas(10);
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setColor("Rojo");
+        vehiculo.setFuncional(true);
+        vehiculo.setMarca("Tesla");
+        persona.setVehiculo(vehiculo);
 
     }
 }
